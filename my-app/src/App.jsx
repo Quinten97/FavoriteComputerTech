27d77a954ba2727cb64b0aefc4fa54d985e612f8
Quinten Favorite <quintenfavorite97@gmail.com>
2022-03-15 16:52:37 -0400

    Created search bar added useEffect for initial fetch

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 function App() {
   let [data, setData] = useState(null);
+  let [query, setQuery] = useState(null);
 
   let getAllData = () => {
     fetch("http://localhost:3000/tickets")
@@ -13,63 +13,76 @@ function App() {
       .then((data) => setData(data));
   };
 
-  const toggle = () => {
-    const table = document.getElementById("table");
-    table.style.display = "block";
+  let getSearchedData = async () => {
+    setQuery(document.getElementById("search-bar").value);
+
+    fetch(`http://localhost:3000/tickets/${query}`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => setData(data));
   };
 
-  let clickHandler = () => {
+  useEffect(() => {
     getAllData();
-    toggle();
-  };
+  }, []);
 
   return (
     <div>
       <header>
         <nav>
-          <a>Home</a>
+          <a href="">Home</a>
         </nav>
         <img />
       </header>
       <section>
-        <button onClick={() => clickHandler()}>Search Tickets</button>
+        <div className="button-container">
+          <form>
+            <input type="text" placeholder="search tickets" id="seach-bar" />
+            <button onSubmit={getSearchedData()}>Submit</button>
+          </form>
+          <button>Create Ticket</button>
+        </div>
         <div className="table-container">
           <table id="table">
-            <tr>
-              <th>id:</th>
-              <th>Date:</th>
-              <th>First Name:</th>
-              <th>Last Name:</th>
-              <th>Email:</th>
-              <th>Phone Number:</th>
-              <th>Brand/Model:</th>
-              <th>Serial:</th>
-              <th>Issue:</th>
-              <th>Notes:</th>
-              <th>Employee:</th>
-            </tr>
+            <thead>
+              <tr>
+                <th>id:</th>
+                <th>Date:</th>
+                <th>First Name:</th>
+                <th>Last Name:</th>
+                <th>Email:</th>
+                <th>Phone Number:</th>
+                <th>Brand/Model:</th>
+                <th>Serial:</th>
+                <th>Issue:</th>
+                <th>Notes:</th>
+                <th>Employee:</th>
+              </tr>
+            </thead>
             {data !== null
               ? data.map((element) => {
                   return (
-                    <tr>
-                      <td>{element.id}</td>
-                      <td>{element.date}</td>
-                      <td>{element.first_name}</td>
-                      <td>{element.last_name}</td>
-                      <td>{element.email}</td>
-                      <td>{element.phone_number}</td>
-                      <td>{element.brand_model}</td>
-                      <td>{element.serial}</td>
-                      <td>{element.issue}</td>
-                      <td>{element.notes}</td>
-                      <td>{element.employee}</td>
-                    </tr>
+                    <tbody>
+                      <tr>
+                        <td>{element.id}</td>
+                        <td>{element.date}</td>
+                        <td>{element.first_name}</td>
+                        <td>{element.last_name}</td>
+                        <td>{element.email}</td>
+                        <td>{element.phone_number}</td>
+                        <td>{element.brand_model}</td>
+                        <td>{element.serial}</td>
+                        <td>{element.issue}</td>
+                        <td>{element.notes}</td>
+                        <td>{element.employee}</td>
+                      </tr>
+                    </tbody>
                   );
                 })
               : ""}
           </table>
         </div>
-        <button>Create Ticket</button>
       </section>
       <footer>
         <p>Favorite Computer Technicians</p>
