fa72816d5252f8dbe948cfc99dedbeaa431a09ef
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2022-03-16 00:06:40 -0400

    added post functionality

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 function App() {
-  let [data, setData] = useState(null);
-  let [query, setQuery] = useState("");
+  const [data, setData] = useState(null);
+  const [query, setQuery] = useState("");
+  const [postData, setPostData] = useState({});
 
-  let getSearchedData = async () => {
+  const getSearchedData = async () => {
     fetch(`http://localhost:3000/tickets/${query}`)
       .then((data) => {
         return data.json();
@@ -13,10 +14,31 @@ function App() {
       .then((data) => setData(data));
   };
 
+  const postTicketData = (ticketData) => {
+    fetch('http://localhost:3000/tickets', {
+      method:"POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(ticketData)
+    })
+    .then(alert('Ticket Created'))
+    .catch(err => console.warn(err))
+  };
+
   const handleSearch = async (event) => {
     await setQuery(event.target.value.toLowerCase());
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target);
+    const data = Array.from(formData.entries())
+    let ticketData ={}
+    data.forEach(([name, value]) => ticketData[name]= value.toLowerCase()) 
+    postTicketData(ticketData);
+  };
+  
   useEffect(() => {
     getSearchedData()
   }, [query]);
@@ -26,31 +48,33 @@ function App() {
       <section>
         <div className="button-container">
           <form>
-            <input type="search" placeholder="search tickets" onChange={handleSearch} value={query}/>
+            <input type="search" placeholder="search tickets" onChange={handleSearch} value={query} />
           </form>
           <button onClick={() => document.getElementById('new-ticket-form').style.display = 'grid'}>Create Ticket</button>
         </div>
-        <form className="new-ticket-form" id="new-ticket-form">
+        <form className="new-ticket-form" id="new-ticket-form" onSubmit={(e) => {
+          handleSubmit(e)
+        }}>
             <label> Date:</label>
-            <input type="date" />
+            <input type="date" name="date"/>
             <label> First Name:</label>
-            <input type="text" />
+            <input type="text" name="firstName"/>
             <label> Last Name:</label>
-            <input type="text" />
+            <input type="text" name="lastName"/>
             <label> Email:</label>
-            <input type="email" />
+            <input type="email" name="email"/>
             <label> Phone Number:</label>
-            <input type="tel" />
+            <input type="tel" name="phoneNumber"/>
             <label> Brand/Model:</label>
-            <input type="text" />
+            <input type="text" name="brandModel"/>
             <label> Serial:</label>
-            <input type="text" />
+            <input type="text" name="serial"/>
             <label> Issue:</label>
-            <textarea cols="30" rows="5" />
+            <textarea cols="30" rows="5" name="issue"/>
             <label> Notes:</label>
-            <textarea cols="30" rows="5" />
+            <textarea cols="30" rows="5" name="notes"/>
             <label> Employee:</label>
-            <input type="text" />
+            <input type="text" name="employee"/>
             <input type='submit' className="ticket-submit" onClick={() => document.getElementById('new-ticket-form').style.display = 'none'}></input>
           </form>
         <div className="table-container">
