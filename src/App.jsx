d0f8a1457dfe33a552a8c96e9196a9b0fa797b92
Quinten Favorite <quintenfavorite97@gmail.com>
2024-02-15 00:25:04 -0500

    changed api host

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   let updateId = 0;
 
-  const baseUrl = "https://ticketing-system-api.vercel.app";
+  const baseUrl = "https://favorite-ticketing-api.onrender.com/";
 
   const getSearchedData = async () => {
     fetch(`${baseUrl}/tickets/${query}`)
